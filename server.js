261f25551ea0d94188becae17ca2ba067416c980
rawera_daniel <rawera.daniel@gmail.com>
2024-08-26 16:56:02 +0300

    feat: connect database with the Express app

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const dotenv = require("dotenv");
-const app = require("./app");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT;
 app.listen(port, () => {
