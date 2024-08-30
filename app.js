e6616cfb741944541a52cc4ce5f0f98680248046
rawera_daniel <rawera.daniel@gmail.com>
2024-08-30 11:25:41 +0300

    feat: implement global error handling middleware to catch errors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const songRouter = require('./routes/songRoutes');
 
 const app = express();
@@ -10,4 +12,6 @@ app.use(express.json());
 // 2) ROUTES
 app.use('/api/v1/songs', songRouter);
 
+// Global error handling middleware
+app.use(globalErrorHandler);
 module.exports = app;
